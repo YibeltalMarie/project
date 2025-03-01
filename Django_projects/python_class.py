a63828d9946f7d74fcbd5e53a8b0bb16283a2c34
yibeltal marie <yibeltalmarie1228@gmail.com>
2025-03-02 02:47:10 +0300

    add middle name

--- Django_projects/python_class.py
+++ Django_projects/python_class.py
@@ -3,6 +3,7 @@ class Name:
   def __init__(self,f_name,l_name):
     self.first_name = f_name
     self.last_name = l_name
+    self.middle_name = "tom"
   
   def print(self):
     print(f"Full Name: {self.first_name}:{self.last_name}")
