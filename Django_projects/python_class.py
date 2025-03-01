a95d014cc2c2a8018e8ebb739634526c2c92434f
yibeltal marie <yibeltalmarie1228@gmail.com>
2025-03-02 02:28:42 +0300

    create two objects

--- Django_projects/python_class.py
+++ Django_projects/python_class.py
@@ -1,8 +1,15 @@
 
-class Animal:
+class Name:
   def __init__(self,f_name,l_name):
     self.first_name = f_name
     self.last_name = l_name
   
   def print(self):
     print(f"Full Name: {self.first_name}:{self.last_name}")
+
+name1 = Name("Yibeltal","Marie")
+name2 = Name("Temesgen","Marie")
+print(name1.first_name)
+print(name2.last_name)
+name1.print()
+name2.print()
